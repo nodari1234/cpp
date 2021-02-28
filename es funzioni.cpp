7d95230e1c448141cceddae0397f88e393fe7f00
Nodari Tsintsadze <tsintsadzenodari66@gmail.co>
2021-02-28 14:18:18 +0100

    ciao

---
+++ es funzioni.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int getArea(int base, int altezza)
+{
+
+ int area;
+
+ area=base*altezza;
+
+ return area;  
+}
+int getArea2(int base, int altezza)
+{
+
+ int area2;
+
+ area2=base*altezza/2;
+
+ return area2;  
+}
+int main(){
+ int area, base, altezza, area2 ;   
+ cout<<"inserire base e altezza del rettangolo : "<<endl;
+ cin>>base;
+ cin>>altezza;
+ area= getArea(base, altezza);    
+ area2=getArea2(base, altezza);
+ cout<<"L'area del rettangolo e' :'' "<<area<<endl;
+ cout<<"L'area del triangolo e' :'' "<<area2<<endl;
+ system("PAUSE");
+
+ return 0;
+}
